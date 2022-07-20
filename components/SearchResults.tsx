33033bb35be2447c6724d6762da0d2fc044e3b8b
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-07-20 09:37:32 -0300

    create app

---
+++ components/SearchResults.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ProductItem from './ProductItem'
+
+interface SearchResultsProps {
+  results: Array<{
+    id: number,
+    price: number,
+    title: string
+  }>
+}
+
+export default function SearchResults({ results }: SearchResultsProps) {
+  return (
+    <div>
+      {results.map(product => {
+        return(
+          <ProductItem 
+            key={product.id}
+            product={product} 
+          />
+        )
+      })}
+    </div>
+  )
+}
